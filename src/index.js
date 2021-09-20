9251380ffaf1fc161db53e69b3ec1c40e1026ab6
Dejwid <dejwidcode@gmail.com>
2021-09-20 21:09:14 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 /* imported modules */
-const Discord = require("discord.js")
+const {Client, Intents} = require("discord.js")
 
 /* config */
 const { prefix, token } = require("../config/config")
 
-const client = new Discord.Client({ partials: ["MESSAGE", "REACTION"] })
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS]
+})
+
+client.on("ready", ()=>{
+    client.user.setActivity(`@${client.user.username} | 1.0.0`)
+    console.log(`${client.user.username} is working!`)
+})
+
+client.on("message", ()=> {
+    
+})
 
 client.on("debug", () => { })
 client.on("error", () => { })
