b913f8901672da26b2b0ba89321d9546c23bd731
Andrew Novak <andrew.novak.developer@gmail.com>
2021-09-21 00:54:12 +0100

    connecting to a voice channel fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* imported modules */
-const {Client, Intents} = require("discord.js")
+const { Client, Intents } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
 require("dotenv").config();
 const { BOT_TOKEN } = process.env;
@@ -7,37 +8,41 @@ const { BOT_TOKEN } = process.env;
 /* config */
 const { prefix } = require("../config/config");
 
-const { joinVoiceChannel } = require('@discordjs/voice');
-
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
-})
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES
+  ]
+});
 
-client.on("ready", ()=>{
-    client.user.setActivity(`@${client.user.username} | 1.0.1`)
-    console.log(`${client.user.username} is working!`)
-})
+client.on("ready", () => {
+  client.user.setActivity(`@${client.user.username} | 1.0.1`);
+  console.log(`${client.user.username} is working!`);
+});
 
-client.on("messageCreate", ( message ) => {
-    if (!message.content.startsWith(prefix) || message.author.bot || !message.guild) return
-    const args = message.content.slice(prefix.length).split(' ')
-    const command = args.shift().toLowerCase()
+client.on("messageCreate", message => {
+  if (
+    !message.content.startsWith(prefix) ||
+    message.author.bot ||
+    !message.guild
+  )
+    return;
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-    if(command == "join")
-    {
-        message.channel.send(`ID: ${message.member.voice.channelId}`)
-        // message.member.voice.channel.join()
-        const connection = joinVoiceChannel({
-            channelId: message.member.voice.channelId,
-            guildId: message.guild.id,
-
-        });
-    }
-})
-
-client.on("message", (message)=>{
-})
-
+  if (command == "join") {
+    const { channelId } = message.member.voice;
+    if (!channelId)
+      return message.channel.send(":warning: You are not in a channel.");
+    const connection = joinVoiceChannel({
+      channelId,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator
+    });
+    message.channel.send("Connecting to the channel");
+  }
+});
 
 client.on("debug", () => {});
 client.on("error", () => {});
